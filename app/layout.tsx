05bbacff929ff040a69caf060f08eb7dbf4bc070
Rafi Chandra <crafi2606@gmail.com>
2025-03-09 05:08:23 +0700

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import { Link } from "@heroui/link";
 import clsx from "clsx";
-
 import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
